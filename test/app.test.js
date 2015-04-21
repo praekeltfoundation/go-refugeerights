02acc9e57b16901ca261cb42092754bcf776e8f7
gsvr <george@westerncapelabs.com>
2015-04-21 13:27:10 +0200

    tests for paginatedchoicestate

--- test/app.test.js
+++ test/app.test.js
@@ -48,6 +48,205 @@ describe("app", function() {
                 });
         });
 
+    // TEST PAGINATEDCHOICESTATE PAGING
+
+        describe("PaginatedChoiceState testing using Refugee Main Menu", function() {
+            describe("forward navigation", function() {
+                it("should display page 1 on arrival", function() {
+                    return tester
+                        .setup.user.addr('082111')
+                        .setup.user.state('state_refugee_main')
+                        .inputs(
+                            {session_event: 'new'}  // dial in
+                        )
+                        // check navigation
+                        .check.interaction({
+                            state: 'state_refugee_main',
+                            reply: [
+                                'MAIN MENU',
+                                '1. New to SA',
+                                '2. The asylum application process',
+                                '3. Asylum applications from children',
+                                '4. Permits',
+                                '5. Support services',
+                                '6. Right to work',
+                                '7. Next'
+                            ].join('\n')
+                        })
+                        .run();
+                });
+
+                it("should display page 2 on Next", function() {
+                    return tester
+                        .setup.user.addr('082111')
+                        .setup.user.state('state_refugee_main')
+                        .inputs(
+                            {session_event: 'new'}  // dial in
+                            , '7'  // state_refugee_main (Next)
+                        )
+                        // check navigation
+                        .check.interaction({
+                            state: 'state_refugee_main',
+                            reply: [
+                                'MAIN MENU',
+                                '1. Health rights',
+                                '2. Education',
+                                '3. Social services',
+                                '4. Banking',
+                                '5. Tips page',
+                                '6. Helpful contact numbers',
+                                '7. Safety concerns',
+                                '8. Next',
+                                '9. Back'
+                            ].join('\n')
+                        })
+                        .run();
+                });
+
+                it("should display page 3 on Next", function() {
+                    return tester
+                        .setup.user.addr('082111')
+                        .setup.user.state('state_refugee_main')
+                        .inputs(
+                            {session_event: 'new'}  // dial in
+                            , '7'  // state_refugee_main (Next p1)
+                            , '8'  // state_refugee_main (Next p2)
+                        )
+                        // check navigation
+                        .check.interaction({
+                            state: 'state_refugee_main',
+                            reply: [
+                                'MAIN MENU',
+                                '1. Statelessness',
+                                '2. LGBTI rights',
+                                '3. Violence against women',
+                                '4. Word definitions',
+                                '5. More word definitions',
+                                '6. Change settings',
+                                '7. Next',
+                                '8. Back'
+                            ].join('\n')
+                        })
+                        .run();
+                });
+
+                it("should display page 4 on Next", function() {
+                    return tester
+                        .setup.user.addr('082111')
+                        .setup.user.state('state_refugee_main')
+                        .inputs(
+                            {session_event: 'new'}  // dial in
+                            , '7'  // state_refugee_main (Next p1)
+                            , '8'  // state_refugee_main (Next p2)
+                            , '7'  // state_refugee_main (Next p3)
+                        )
+                        // check navigation
+                        .check.interaction({
+                            state: 'state_refugee_main',
+                            reply: [
+                                'MAIN MENU',
+                                '1. Ts & Cs of this service',
+                                '2. About LHR',
+                                '3. Back'
+                            ].join('\n')
+                        })
+                        .run();
+                });
+            });
+
+            describe("backward navigation", function() {
+                it("should display page 3 on Back", function() {
+                    return tester
+                        .setup.user.addr('082111')
+                        .setup.user.state('state_refugee_main')
+                        .inputs(
+                            {session_event: 'new'}  // dial in
+                            , '7'  // state_refugee_main (Next p1)
+                            , '8'  // state_refugee_main (Next p2)
+                            , '7'  // state_refugee_main (Next p3)
+                            , '3'  // state_refugee_main (Back p4)
+                        )
+                        // check navigation
+                        .check.interaction({
+                            state: 'state_refugee_main',
+                            reply: [
+                                'MAIN MENU',
+                                '1. Statelessness',
+                                '2. LGBTI rights',
+                                '3. Violence against women',
+                                '4. Word definitions',
+                                '5. More word definitions',
+                                '6. Change settings',
+                                '7. Next',
+                                '8. Back'
+                            ].join('\n')
+                        })
+                        .run();
+                });
+
+                it("should display page 2 on Back", function() {
+                    return tester
+                        .setup.user.addr('082111')
+                        .setup.user.state('state_refugee_main')
+                        .inputs(
+                            {session_event: 'new'}  // dial in
+                            , '7'  // state_refugee_main (Next p1)
+                            , '8'  // state_refugee_main (Next p2)
+                            , '7'  // state_refugee_main (Next p3)
+                            , '3'  // state_refugee_main (Back p4)
+                            , '8'  // state_refugee_main (Back p3)
+                        )
+                        // check navigation
+                        .check.interaction({
+                            state: 'state_refugee_main',
+                            reply: [
+                                'MAIN MENU',
+                                '1. Health rights',
+                                '2. Education',
+                                '3. Social services',
+                                '4. Banking',
+                                '5. Tips page',
+                                '6. Helpful contact numbers',
+                                '7. Safety concerns',
+                                '8. Next',
+                                '9. Back'
+                            ].join('\n')
+                        })
+                        .run();
+                });
+
+                it("should display page 1 on Back", function() {
+                    return tester
+                        .setup.user.addr('082111')
+                        .setup.user.state('state_refugee_main')
+                        .inputs(
+                            {session_event: 'new'}  // dial in
+                            , '7'  // state_refugee_main (Next p1)
+                            , '8'  // state_refugee_main (Next p2)
+                            , '7'  // state_refugee_main (Next p3)
+                            , '3'  // state_refugee_main (Back p4)
+                            , '8'  // state_refugee_main (Back p3)
+                            , '9'  // state_refugee_main (Back p3)
+                        )
+                        // check navigation
+                        .check.interaction({
+                            state: 'state_refugee_main',
+                            reply: [
+                                'MAIN MENU',
+                                '1. New to SA',
+                                '2. The asylum application process',
+                                '3. Asylum applications from children',
+                                '4. Permits',
+                                '5. Support services',
+                                '6. Right to work',
+                                '7. Next'
+                            ].join('\n')
+                        })
+                        .run();
+                });
+            });
+        });
+
 
     // TEST REGISTRATION
 
@@ -427,6 +626,35 @@ describe("app", function() {
                             'info info info info info info info info info info info info info info info info info info',
                             '1. Back',
                             '2. Exit'
+                        ].join('\n')
+                    })
+                    .run();
+            });
+
+            it("should take them to migrant main menu if they choose exit", function() {
+                return tester
+                    .setup.user.addr('082111')
+                    .inputs(
+                        {session_event: 'new'}  // dial in
+                        , '2'  // state_language (french)
+                        , '8'  // state_country (next page)
+                        , '6'  // state_country (syria)
+                        , '2'  // state_status (who is migrant)
+                        , '1'  // state_who_migrant (yes - migrant)
+                        , '2'  // state_migrant_rights_info (exit)
+                    )
+                    // check navigation
+                    .check.interaction({
+                        state: 'state_migrant_main',
+                        reply: [
+                            'MAIN MENU',
+                            '1. New to SA',
+                            '2. The visa application process',
+                            '3. Unaccompanied / separated children',
+                            '4. Support services',
+                            '5. Employment',
+                            '6. Healthcare',
+                            '7. Next'
                         ].join('\n')
                     })
                     .run();
